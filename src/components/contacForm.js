7e063528fb7669503cee3c48e196a419b0c7eaa8
Bayram Çınar <bayram200212@gmail.com>
2024-08-24 22:00:29 +0300

    update

--- src/components/contacForm.js
+++ src/components/contacForm.js
@@ -131,7 +131,7 @@ function ContactForm() {
         </div>
         <button
           type="submit"
-          className="px-4 py-2 bg-blue-500 text-white font-semibold rounded-md shadow-sm hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+          className="px-4 w-full text-center py-2 bg-blue-500 text-white font-semibold rounded-md shadow-sm hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
         >
           Gönder
         </button>
